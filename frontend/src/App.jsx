cecc6df275b1acf89918bf9d931a86a9a903e457
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-11-20 23:44:55 +0530

    Added freebok component and also slider in that using React slick website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Freebook from "./components/Freebook";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
      <Navbar/>
      <Banner/>
+     <Freebook/>
      <Footer/>
     </>
   );
