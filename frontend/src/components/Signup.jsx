3e33d1c98c2d82ff7c20316c0459d3c34a4df7fe
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-11-22 16:55:56 +0530

    Added signup functionality

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Login from "./Login";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm();
+    
+      const onSubmit = (data) => console.log(data);
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <div className="w-[600px]">
           <div className="modal-box">
-            <form method="dialog">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
               {/* if there is a button in form, it will close the modal */}
               <Link
                 to="/"
@@ -26,7 +35,14 @@ const Signup = () => {
                 type="text"
                 placeholder="Enter your fullname"
                 className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("fullname", { required: true })}
               />
+                <br />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                    </span>
+                )}
             </div>
             {/* Email */}
             <div className="mt-4 space-y-2">
@@ -36,7 +52,14 @@ const Signup = () => {
                 type="email"
                 placeholder="Enter your email"
                 className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
               />
+              <br />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
             </div>
             {/* password */}
             <div className="mt-4 space-y-2">
@@ -46,7 +69,14 @@ const Signup = () => {
                 type="password"
                 placeholder="Enter your password"
                 className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("password", { required: true })}
               />
+              <br />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
             </div>
             {/* Button */}
             <div className="flex justify-around mt-4">
